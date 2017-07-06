be85bdccdbec182e184416587d864ac107b3e6d0
collin person and Kate Byars <collin@>
2017-07-06 09:48:20 -0700

    modify css, index and js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+  $("#formOne").submit(function()  {
+    var nameInput = $("input#name").val();
+    var favoritePlaceInput = $("input#favoritePlace").val();
+    var nounInput = $("input#noun").val();
+    var musicInput = $("input#music").val();
+    var foodInput = $("input#food").val();
+    var vacationInput = $("input#vacation").val();
+
+    event.preventDefault();
+  });
+});
